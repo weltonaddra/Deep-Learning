06df69d4b2aca540c21e400fe5378ca37863c259
Welton Addra <weltonaddra11@gmail.com>
2025-10-30 19:03:26 -0400

    Clarify BATCH_SIZE definition in config.py
    Added comment to clarify the purpose of BATCH_SIZE.

--- Class Files/config.py
+++ Class Files/config.py
@@ -1,13 +1,13 @@
 import os
 
 class Config:
-    """Global configuration constants."""
+# Used for transforming images to fit the expected input aspec ratio
     IMAGENET_MEAN = [0.485, 0.456, 0.406]
     IMAGENET_STD  = [0.229, 0.224, 0.225]
     PAD_FILL_RGB  = tuple(int(m * 255) for m in IMAGENET_MEAN)
 
     DATA_DIR   = os.path.join("Class Files" , "chest_xray")
-    BATCH_SIZE = 6
+    BATCH_SIZE = 6  # Number of examples in a batch 
     NUM_WORKERS = 0
     IMG_SIZE = 224
 
